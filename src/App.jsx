0e5b15b1eae0c807e043e1ec818d47afcb5ea703
AndreeaSichitiu <sichitiuandreea@gmail.com>
2024-01-17 21:15:19 +0200

    intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-import React from 'react';
+import Intro from 'components/Intro/Intro';
 
 export const App = () => {
   return (
-    <div>
-      <h1>TaskPro-echipa 2</h1>
-      <h2>Membri</h2>
-      <p>Andreea Sichitiu</p>
-      <p>Andreea Tarca</p>
-      <p>Anisoara Cimpeanu</p>
-      <p>Daniela Sarau</p>
-      <p>Claudia Papp</p>
-      <p>Costel Amariutei</p>
-      <p>Alin Lechintan</p>
-    </div>
+    <>
+      <Intro></Intro>
+    </>
   );
 };
+
+export default App;
