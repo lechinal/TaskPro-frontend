0340f4ba921cc7fb35b84b9e76ed442e06e2e6b6
lechinal <alin.lechintan@outlook.com>
2024-01-17 14:53:14 +0200

    up

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const App = () => {
+  return (
+    <div>
+      <h1>TaskPro-echipa 2</h1>
+      <h2>Membri</h2>
+      <p>Andreea Sichitiu</p>
+      <p>Andreea Tarca</p>
+      <p>Anisoara Cimpeanu</p>
+      <p>Daniela Sarau</p>
+      <p>Claudia Papp</p>
+      <p>Costel Amariutei</p>
+      <p>Alin Lechintan</p>
+    </div>
+  );
+};
