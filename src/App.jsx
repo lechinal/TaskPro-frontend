e70231fca405f66999d491ffda1edbf7bfb17a9a
lechinal <alin.lechintan@outlook.com>
2024-01-18 11:00:33 +0200

    update
    Routes pt paginile principale

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import WelcomePage from 'components/WelcomePage/WelcomePage';
+import WelcomePage from 'pages/WelcomePage/WelcomePage';
+import AuthPage from 'pages/AuthPage/AuthPage';
+import HomePage from 'pages/HomePage/HomePage';
+import BoardPage from 'pages/BoardPage/BoardPage';
+
+import { Routes, Route } from 'react-router-dom';
 
 export const App = () => {
   return (
     <>
-      <WelcomePage />
+      <Routes>
+        <Route path="/welcome" element={<WelcomePage />} />
+        <Route path="/auth/:id" element={<AuthPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/home/:boardName" element={<BoardPage />} />
+      </Routes>
     </>
   );
 };
