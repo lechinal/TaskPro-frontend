2505d3f3feddaf54329a23514dc419f7aa2688ea
Alin Lechintan <122990717+lechinal@users.noreply.github.com>
2024-01-25 09:51:34 +0200

    Update authOperations.js

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
+axios.defaults.baseURL = 'http://localhost:4000//';
 
 // To add JWT
 const setAuthHeader = token => {
