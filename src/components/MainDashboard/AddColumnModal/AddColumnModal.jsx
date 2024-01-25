10116db7799bb9f14fa219b120099ee0197c0976
Alin Lechintan <122990717+lechinal@users.noreply.github.com>
2024-01-25 12:29:22 +0200

    Update AddColumnModal.jsx

--- src/components/MainDashboard/AddColumnModal/AddColumnModal.jsx
+++ src/components/MainDashboard/AddColumnModal/AddColumnModal.jsx
@@ -117,15 +117,6 @@ function AddColumnModal({ closeModal }) {
             </div>
           </div>
         </div>
-        <Button className={css.addColumnBtn}>
-          <svg className={`${css.iconPlus} ${css.iconPlusBlack}`}>
-            <use href={`${icons}#icon-plus-black`} />
-          </svg>
-          <svg className={`${css.iconPlus} ${css.iconPlusWhite}`}>
-            <use href={`${icons}#icon-plus`} />
-          </svg>
-          <p>Add another card</p>
-        </Button>
       </div>
     </ThemeProvider>
   );
