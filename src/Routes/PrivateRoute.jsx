a4e7ef80d36fab78148e869c0376482515a486c4
daniellesarau <123309317+daniellesarau@users.noreply.github.com>
2024-01-25 16:32:09 +0200

    app

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,10 +1,15 @@
-import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
-import { selectIsLoggedIn } from '../redux/auth/authSelectors';
+import { useSelector } from 'react-redux';
+import {
+  selectIsLoggedIn,
+  selectIsRefreshing,
+} from '../redux/auth/authSelectors';
 
 const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
-  return !isLoggedIn ? <Navigate to={redirectTo} /> : Component;
-};
+  const isRefreshing = useSelector(selectIsRefreshing);
 
+  const shouldRedirect = !isLoggedIn && !isRefreshing;
+  return shouldRedirect ? <Navigate to={redirectTo} /> : Component;
+};
 export default PrivateRoute;
